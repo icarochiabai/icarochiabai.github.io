ca275f195384acd4fc86b55943669dfac6981b80
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-05-01 13:47:39 -0300

    fix:wait window mount to handle resize

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from 'next/head'
+import { useEffect } from 'react';
         
-export default function Home() {
-  let resizeTimer;
-  window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 400);
-  });
+export default function LandingPage() {
+  useEffect(() => {
+    let resizeTimer;
+    window.addEventListener("resize", () => {
+      document.body.classList.add("resize-animation-stopper");
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+      }, 400);
+    });
+  }, []);
+
   return (
     <>
       <Head>
